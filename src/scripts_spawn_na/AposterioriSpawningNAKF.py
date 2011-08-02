ea0de2faca8c1701e0206ed1eba38ba8b78dab6a
raoulbq <raoulb@student.ethz.ch>
2011-08-02 12:45:12 +0000

    -> Experimental k-free parameter estimation and spawning
    -> Note: Needs new quadrature features

---
+++ src/scripts_spawn_na/AposterioriSpawningNAKF.py
@@ -0,0 +1,238 @@
+"""The WaveBlocks Project
+
+Script to spawn new wavepackets aposteriori to an already completed simulation.
+This can be used to evaluate spawning errors and test criteria for finding the
+best spawning time.
+
+@author: R. Bourquin
+@copyright: Copyright (C) 2011 R. Bourquin
+@license: Modified BSD License
+"""
+
+import sys
+
+import numpy as np
+from scipy import linalg as spla
+
+from WaveBlocks import ParameterProvider
+from WaveBlocks import IOManager
+from WaveBlocks import PotentialFactory
+from WaveBlocks import HagedornWavepacket
+
+from NonAdiabaticSpawnerKF import NonAdiabaticSpawnerKF
+
+
+def aposteriori_spawning(fin, fout, pin, pout, save_canonical=False):
+    """
+    @param f: An I{IOManager} instance providing the simulation data.
+    @keyword datablock: The data block where the results are.
+    """
+    # Number of time steps we saved
+    timesteps = fin.load_wavepacket_timegrid()
+    nrtimesteps = timesteps.shape[0]
+
+    params = fin.load_wavepacket_parameters()
+    coeffs = fin.load_wavepacket_coefficients()
+
+    # A data transformation needed by API specification
+    coeffs = [ [ coeffs[i,j,:] for j in xrange(pin["ncomponents"]) ] for i in xrange(nrtimesteps) ]
+
+    # The potential
+    Potential = PotentialFactory.create_potential(pin)
+
+    # Initialize a mother Hagedorn wavepacket with the data from another simulation
+    HAWP = HagedornWavepacket(pin)
+    HAWP.set_quadrator(None)
+
+    # Initialize an empty wavepacket for spawning
+    SWP = HagedornWavepacket(pout)
+    SWP.set_quadrator(None)
+
+    # Initialize a Spawner
+    NAS = NonAdiabaticSpawnerKF(pout)
+
+    # Try spawning for these components, if none is given, try it for all.
+    if not "spawn_components" in parametersout:
+        components = range(pin["ncomponents"])
+    else:
+        components = parametersout["spawn_components"]
+
+    # Iterate over all timesteps and spawn
+    for i, step in enumerate(timesteps):
+        print(" Try spawning at timestep "+str(step))
+
+        # Configure the wave packet and project to the eigenbasis.
+        HAWP.set_parameters(params[i])
+        HAWP.set_coefficients(coeffs[i])
+
+        # Project to the eigenbasis as the parameter estimation
+        # has to happen there because of coupling.
+        T = HAWP.clone()
+        T.project_to_eigen(Potential)
+
+        # Try spawning a new packet for each component
+        estps = [ NAS.estimate_parameters(T, component=acomp) for acomp in components ]
+
+        for index, ps in enumerate(estps):
+            if ps is not None:
+                # One choice of the sign
+                U = SWP.clone()
+                U.set_parameters(ps)
+                # Project the coefficients to the spawned packet
+                tmp = T.clone()
+                NAS.project_coefficients(tmp, U, component=components[index])
+
+                # Other choice of the sign
+                V = SWP.clone()
+                # Transform parameters
+                psm = list(ps)
+                B = ps[0]
+                Bm = -np.real(B)+1.0j*np.imag(B)
+                psm[0] = Bm
+                V.set_parameters(psm)
+                # Project the coefficients to the spawned packet
+                tmp = T.clone()
+                NAS.project_coefficients(tmp, V, component=components[index])
+
+                # Compute some inner products to finally determine which parameter set we use
+                parameters = {"eps":SWP.eps}
+                ou = abs(inner(parameters, T, U))
+                ov = abs(inner(parameters, T, V))
+
+                # Choose the packet which maximizes the inner product.
+                # This is the main point!
+                if ou >= ov:
+                    U = U
+                else:
+                    U = V
+
+                # Finally do the spawning, this is essentially to get the remainder T right
+                # The packet U is already ok by now.
+                NAS.project_coefficients(T, U, component=components[index])
+
+                # Transform back
+                if save_canonical is True:
+                    T.project_to_canonical(Potential)
+                    U.project_to_canonical(Potential)
+
+                # Save the mother packet rest
+                fout.save_wavepacket_parameters(T.get_parameters(), timestep=step, block=2*index)
+                fout.save_wavepacket_coefficients(T.get_coefficients(), timestep=step, block=2*index)
+
+                # Save the spawned packet
+                fout.save_wavepacket_parameters(U.get_parameters(), timestep=step, block=2*index+1)
+                fout.save_wavepacket_coefficients(U.get_coefficients(), timestep=step, block=2*index+1)
+
+
+
+def inner(parameters, P1, P2, QR=None):
+    """Compute the inner product between two wavepackets.
+    """
+    # todo: Refactor this and put it into an own file/class!
+
+    # Quadrature for <orig, s1>
+    c1 = P1.get_coefficients(component=0)
+    c2 = P2.get_coefficients(component=0)
+
+    # Assuming same quadrature rule for both packets
+    # Implies same basis size!
+    if QR is None:
+        QR = P1.get_quadrator().get_qr()
+
+    # Mix the parameters for quadrature
+    (Pm, Qm, Sm, pm, qm) = P1.get_parameters()
+    (Ps, Qs, Ss, ps, qs) = P2.get_parameters()
+
+    rm = Pm/Qm
+    rs = Ps/Qs
+
+    r = np.conj(rm)-rs
+    s = np.conj(rm)*qm - rs*qs
+
+    q0 = np.imag(s) / np.imag(r)
+    Q0 = -0.5 * np.imag(r)
+    QS = 1 / np.sqrt(Q0)
+
+    # The quadrature nodes and weights
+    nodes = q0 + parameters["eps"] * QS * QR.get_nodes()
+    weights = QR.get_weights()
+
+    # Basis sets for both packets
+    basis_1 = P1.evaluate_base_at(nodes, prefactor=True)
+    basis_2 = P2.evaluate_base_at(nodes, prefactor=True)
+
+    R = QR.get_order()
+
+    # Loop over all quadrature points
+    tmp = 0.0j
+    for r in xrange(R):
+        tmp += np.conj(np.dot( c1[:,0], basis_1[:,r] )) * np.dot( c2[:,0], basis_2[:,r]) * weights[0,r]
+    result = parameters["eps"] * QS * tmp
+
+    return result
+
+
+
+
+if __name__ == "__main__":
+    # Input data manager
+    iomin = IOManager()
+
+    # Read file with simulation data
+    try:
+        iomin.open_file(filename=sys.argv[1])
+    except IndexError:
+        iomin.open_file()
+
+    # Read a configuration file with the spawn parameters
+    try:
+        parametersspawn = ParameterProvider()
+        parametersspawn.read_parameters(sys.argv[2])
+    except IndexError:
+        raise IOError("No spawn configuration given!")
+
+    parametersin = iomin.get_parameters()
+
+    # Check if we can start a spawning simulation
+    if parametersin["algorithm"] != "hagedorn":
+        iomin.finalize()
+        raise ValueError("Unknown propagator algorithm.")
+
+    # Parameters for spawning simulation
+    parametersout = ParameterProvider()
+
+    # Transfer the simulation parameters
+    parametersout.set_parameters(parametersin)
+
+    # And add spawning related configurations variables
+    parametersout.update_parameters(parametersspawn)
+
+    # How much time slots do we need
+    tm = parametersout.get_timemanager()
+    slots = tm.compute_number_saves()
+
+    # Second IOM for output data of the spawning simulation
+    iomout = IOManager()
+    iomout.create_file(parametersout, filename="simulation_results_spawn.hdf5")
+
+    # Allocate all the data blocks
+    iomout.add_grid(parametersout)
+    iomout.save_grid(iomin.load_grid())
+    iomout.add_wavepacket(parametersin)
+
+    for i in xrange(1,2*len(parametersout["spawn_components"])):
+        iomout.create_block()
+        iomout.add_grid_reference(blockfrom=i, blockto=0)
+        if i % 2 == 0:
+            # Block for remainder / mother after spawning
+            iomout.add_wavepacket(parametersin, block=i)
+        else:
+            # Block for spawned packet
+            iomout.add_wavepacket(parametersout, block=i)
+
+    # Really do the aposteriori spawning simulation
+    aposteriori_spawning(iomin, iomout, parametersin, parametersout)
+
+    # Close the inpout/output files
+    iomin.finalize()
+    iomout.finalize()
---
+++ src/scripts_spawn_na/NonAdiabaticSpawnerKF.py
@@ -0,0 +1,264 @@
+"""The WaveBlocks Project
+
+This file conatins the code for spawning new wavepackets depending
+on some criterion.
+
+@author: R. Bourquin
+@copyright: Copyright (C) 2011 R. Bourquin
+@license: Modified BSD License
+"""
+
+import numpy as np
+from scipy import linalg as spla
+
+from WaveBlocks import Spawner
+
+
+class NonAdiabaticSpawnerKF(Spawner):
+    """This class implements parameter estimation and basis
+    projection for spawning of Hagedorn wavepackets in the
+    non-adiabatic case.
+    """
+
+    def __init__(self, parameters):
+        # Cache some parameter values for efficiency
+        self.parameters = parameters
+
+        # Configuration parameters related to spawning
+        self.eps = parameters["eps"]
+        self.threshold = parameters["spawn_threshold"]
+
+        # The spawning method used, default is "lumping"
+        self.spawn_method = parameters["spawn_method"] if "spawn_method" in parameters else "lumping"
+
+        # The value of k for the (2*k+1) in the parameter estimation formula
+        # Do not confuse this with the K in the adiabatic spawning!
+        # The default 0 Corresponds to a Gaussian.
+        self.order = parameters["spawn_order"] if "spawn_order" in parameters else 0
+
+        # Only used for spawning method "projection"
+        self.max_order = parameters["spawn_max_order"] if "spawn_max_order" in  parameters else 1
+
+
+    def estimate_parameters(self, packet, component=0, order=None):
+        """Compute the parameters for a new wavepacket.
+        """
+        if order is None:
+            order = self.order
+
+        P, Q, S, p, q = packet.get_parameters(component=component)
+
+        c = packet.get_coefficients(component=component)
+        c = np.squeeze(c)
+
+        w = spla.norm(c)**2
+
+        if w < self.threshold**2:
+            print(" Warning: really small w! Nothing to spawn!")
+            return None
+
+        # Compute spawning position and impulse
+        k = np.arange(1, packet.get_basis_size())
+        ck   = c[1:]
+        ckm1 = c[:-1]
+        tmp = np.sum( np.conj(ck) * ckm1 * np.sqrt(k) )
+
+        a = q + np.sqrt(2)*self.eps/w * np.real( Q * tmp )
+        b = p + np.sqrt(2)*self.eps/w * np.real( P * tmp )
+
+
+        # # Diagonal parts
+        # k = np.arange(0, packet.get_basis_size())
+        # ck = c[:]
+        # theta1 = np.sum( np.abs(ck)**2 * (2.0*k + 1.0) )
+
+        # # First off-diagonal parts
+        # k = np.arange(0, packet.get_basis_size()-2)
+        # ck   = c[:-2]
+        # ckp2 = c[2:]
+        # theta2 = np.sum( np.conj(ckp2) * ck * np.sqrt((k+1)*(k+2)) )
+
+        # # Compute other parameters
+        # tmpA = self.eps**2/2 * ( abs(Q)**2 * theta1 + 2.0*np.real(Q**2 * theta2) )
+        # tmpB = self.eps**2/2 * ( abs(P)**2 * theta1 + 2.0*np.real(P**2 * theta2) )
+
+
+        # # Diagonal parts
+        # k = np.arange(0, packet.get_basis_size())
+        # ck = c[:]
+        # theta0 = np.sum( np.abs(ck)**2 * (6.0*k**2 + 6.0*k + 3.0) )
+
+        # # Second off-diagonal parts
+        # k = np.arange(0, packet.get_basis_size()-2)
+        # ck   = c[:-2]
+        # ckp2 = c[2:]
+        # theta2 = np.sum( np.conj(ckp2) * ck * (4.0*k + 6.0) * np.sqrt((k+1)*(k+2)) )
+
+        # # Fourth off-diagonal parts
+        # k = np.arange(0, packet.get_basis_size()-4)
+        # ck   = c[:-4]
+        # ckp4 = c[4:]
+        # theta4 = np.sum( np.conj(ckp4) * ck * np.sqrt((k+1)*(k+2)*(k+3)*(k+4)) )
+
+        # tmpA2 = self.eps**4/4 * ( abs(Q)**4 * theta0 + 2.0*np.real( Q**3*np.conj(Q) * theta2 + Q**4 * theta4 ) )
+        # tmpB2 = self.eps**4/4 * ( abs(P)**4 * theta0 + 2.0*np.real( P**3*np.conj(P) * theta2 + P**4 * theta4 ) )
+
+        #alpha0 = 1.0
+        #alpha2 = tmpA
+        #alpha4 = tmpA2
+
+        # Compute other parameters
+        #Aabs2 = -2.0/self.eps**2 * (q-a)**2 + 1.0/w * ( abs(Q)**4 * theta0 + 2.0*np.real( Q**3*np.conj(Q) * theta2 + Q**4 * theta4 ) )
+        #Babs2 = -2.0/self.eps**2 * (p-b)**2 + 1.0/w * ( abs(P)**4 * theta0 + 2.0*np.real( P**3*np.conj(P) * theta2 + P**4 * theta4 ) )
+        #Aabs2 = -2.0/self.eps**2 * (q-a)**2 + 1.0/w * ( abs(Q)**4 * theta0 + 2.0*np.real( Q**3*np.conj(Q) * theta2 + Q**4 * theta4 ) )
+        #Babs2 = -2.0/self.eps**2 * (p-b)**2 + 1.0/w * ( abs(P)**4 * theta0 + 2.0*np.real( P**3*np.conj(P) * theta2 + P**4 * theta4 ) )
+
+        # Try it with numerical quadrature
+        f0 = lambda x: np.ones(x.shape)
+        f2 = lambda x: (x-q)**2
+        f4 = lambda x: (x-q)**4
+
+        quadrature = packet.get_quadrature()
+
+        alpha0 = quadrature.quadrate(packet, f0, summed=True)
+        alpha2 = 2/self.eps**2 * quadrature.quadrate(packet, f2, summed=True)
+        alpha4 = 4/self.eps**4 * quadrature.quadrate(packet, f4, summed=True)
+
+        Aabs2 = (alpha2 + np.sqrt(0.0 + alpha2**2 - alpha0*alpha4)) / (alpha0)
+
+
+        fpacket = packet.to_fourier_space(assign=False)
+
+        f0 = lambda x: np.ones(x.shape)
+        f2 = lambda x: (x-p)**2
+        f4 = lambda x: (x-p)**4
+
+        beta0 = quadrature.quadrate(fpacket, f0, summed=True)
+        beta2 = 2/self.eps**2 * quadrature.quadrate(fpacket, f2, summed=True)
+        beta4 = 4/self.eps**4 * quadrature.quadrate(fpacket, f4, summed=True)
+
+        Babs2 = (beta2 + np.sqrt(0.0 + beta2**2 - beta0*beta4)) / (beta0)
+
+        print((alpha0, alpha2, alpha4))
+        print((beta0, beta2, beta4))
+
+        # Transform
+        #A = np.sqrt(Aabs2)
+        #B = (np.sqrt(Aabs2 * Babs2 - 1.0 + 0.0j) + 1.0j) / A
+
+        A = Aabs2
+        B = Babs2
+
+
+        return (B, A, S, b, a)
+
+
+    def project_coefficients(self, mother, child, component=0, order=None):
+        """Update the superposition coefficients of mother and
+        spawned wavepacket. Here we decide which method to use
+        and call the corresponding method.
+        """
+        if self.spawn_method == "lumping":
+            return self.spawn_lumping(mother, child, component, order=order)
+        elif self.spawn_method == "projection":
+            return self.spawn_basis_projection(mother, child, component, order=order)
+        else:
+            raise ValueError("Unknown spawning method '" + self.spawn_method + "'!")
+
+
+    def spawn_lumping(self, mother, child, component, order=None):
+        """Update the superposition coefficients of mother and
+        spawned wavepacket. We produce just a gaussian which
+        takes the full norm <w|w> of w.
+        """
+        if order is None:
+            order = self.order
+
+        c_old = mother.get_coefficients(component=component)
+        w = spla.norm(np.squeeze(c_old))
+
+        # Mother packet
+        c_new_m = np.zeros(c_old.shape, dtype=np.complexfloating)
+
+        # Spawned packet
+        c_new_s = np.zeros((child.get_basis_size(),1), dtype=np.complexfloating)
+        # Pure $\phi_order$ function
+        c_new_s[order,0] = 1.0
+        # But normalized
+        c_new_s = w * c_new_s
+
+        mother.set_coefficients(c_new_m, component=component)
+        child.set_coefficients(c_new_s, component=component)
+
+        return (mother, child)
+
+
+    def spawn_basis_projection(self, mother, child, component, order=None):
+        """Update the superposition coefficients of mother and
+        spawned wavepacket. We do a full basis projection to the
+        basis of the spawned wavepacket here.
+        """
+        c_old = mother.get_coefficients(component=component)
+
+        # Mother packet
+        c_new_m = np.zeros(c_old.shape, dtype=np.complexfloating)
+
+        # Spawned packet
+        c_new_s = np.zeros((child.get_basis_size(),1), dtype=np.complexfloating)
+
+        # Quadrature rule, assume same quadrature order for both packets
+        QR = mother.get_quadrature().get_qr()
+
+        # Mix the parameters for quadrature
+        (Pm, Qm, Sm, pm, qm) = mother.get_parameters()
+        (Ps, Qs, Ss, ps, qs) = child.get_parameters()
+
+        rm = Pm/Qm
+        rs = Ps/Qs
+
+        r = np.conj(rm)-rs
+        s = np.conj(rm)*qm - rs*qs
+
+        q0 = np.imag(s) / np.imag(r)
+        Q0 = -0.5 * np.imag(r)
+        QS = 1 / np.sqrt(Q0)
+
+        # The quadrature nodes and weights
+        nodes = q0 + self.eps * QS * QR.get_nodes()
+        weights = QR.get_weights()
+
+        # Basis sets for both packets
+        basis_m = mother.evaluate_base_at(nodes, prefactor=True)
+        basis_s = child.evaluate_base_at(nodes, prefactor=True)
+
+        max_order = min(child.get_basis_size(), self.max_order)
+
+        # Project to the basis of the spawned wavepacket
+        # Original, inefficient code for projection
+        # R = QR.get_order()
+        # for i in xrange(max_order):
+        #     # Loop over all quadrature points
+        #     tmp = 0.0j
+        #     for r in xrange(R):
+        #         tmp += np.conj(np.dot( c_old[:,0], basis_m[:,r] )) * basis_s[i,r] * weights[0,r]
+        #     c_new_s[i,0] = self.eps * QS * tmp
+
+        # Optimised and vectorised code (in ugly formatting)
+        c_new_s[:max_order,:] = self.eps * QS * (
+            np.reshape(
+                np.sum(
+                    np.transpose(
+                        np.reshape(
+                            np.conj(
+                                np.sum(c_old[:,:] * basis_m[:,:],axis=0)
+                            ) ,(-1,1)
+                        )
+                    ) * (basis_s[:max_order,:] * weights[:,:]), axis=1
+                ) ,(-1,1)
+            ))
+
+        # Reassign the new coefficients
+        mother.set_coefficients(c_new_m, component=component)
+        child.set_coefficients(c_new_s, component=component)
+
+        return (mother, child)
