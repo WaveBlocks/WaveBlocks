afed5c5f29563445791298e7eb3ef397c1d58bc4
raoulbq <raoulb@student.ethz.ch>
2011-10-08 01:50:45 +0000

    -> Fixed data type inconsistency in Arnoldi iteration

--- src/WaveBlocks/MatrixExponential.py
+++ src/WaveBlocks/MatrixExponential.py
@@ -10,7 +10,7 @@ Pade approximations and an Arnoldi iteration method.
 @license: Modified BSD License
 """
 
-from scipy import zeros, dot, mat, hstack
+from numpy import zeros, hstack, mat, dot, complexfloating
 from scipy.linalg import norm, expm
 
 
@@ -26,20 +26,20 @@ def matrix_exp_pade(A, coefficients, factor):
 def arnoldi(A, v0, k):
     """Arnoldi algorithm (Krylov approximation of a matrix)
     @param A: The matrix to approximate.
-    @param v0: The initial vector (should be in matrix form) 
+    @param v0: The initial vector (should be in matrix form)
     @param k: The number of Krylov steps.
     @return: A tupel (V, H) where V is the matrix (large, N*k) containing the orthogonal vectors and
     H is the matrix (small, k*k) containing the Krylov approximation of A.
     """
-    V = mat( v0.copy() / norm(v0) )
-    H = mat( zeros((k+1,k)) )
+    V = mat(v0.copy() / norm(v0))
+    H = mat(zeros((k+1,k)), dtype=complexfloating)
     for m in xrange(k):
         vt = A * V[:,m]
         for j in xrange(m+1):
             H[j,m] = (V[:,j].H*vt)[0,0]
-            vt -= H[j,m] * V[:,j]        
+            vt -= H[j,m] * V[:,j]
         H[m+1,m] = norm(vt)
-        V = hstack( (V, vt.copy()/H[m+1,m]) ) 
+        V = hstack((V, vt.copy()/H[m+1,m]))
     return (V, H)
 
 
@@ -51,6 +51,6 @@ def matrix_exp_arnoldi(A, v, factor, k):
     @param k: The number of Krylov steps.
     """
     V, H = arnoldi(A, v, k)
-    eH = mat( expm(-1.0j*factor*H[:-1,:]) )
-    r = V[:,:-1] * eH[:,0] 
+    eH = mat(expm(-1.0j*factor*H[:-1,:]))
+    r = V[:,:-1] * eH[:,0]
     return r * norm(v)
