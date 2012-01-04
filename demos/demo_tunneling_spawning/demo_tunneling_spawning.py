2a9c00784bedea96b576de73fcf6dc74ce9f4198
raoulbq <raoulb@student.ethz.ch>
2012-01-04 23:13:05 +0000

    -> New demos showing propagation of spawned packets

---
+++ demos/demo_tunneling_spawning/demo_tunneling_spawning.py
@@ -0,0 +1,25 @@
+algorithm = "spawning_adiabatic"
+#algorithm = "hagedorn"
+
+potential = "eckart"
+
+T = 70
+dt = 0.005
+
+eps = 0.0234218**0.5
+
+basis_size = 300
+
+parameters = [ (0.1935842258501978j, 5.1657101481699996, 0.0, 0.24788547371, -7.55890450883) ]
+coefficients = [[(0, 1.0)]]
+
+leading_component = 0
+
+f = 9.0
+ngn = 4096
+
+write_nth = 20
+
+spawn_condition = "adiabatic_K_threshold"
+spawn_K0 = 100
+spawn_threshold = 0.32
